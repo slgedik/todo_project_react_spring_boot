2589a6c524c00ab42d6a1890554b77185d5c8a9e
slgedik <112625294+slgedik@users.noreply.github.com>
2023-08-31 02:29:57 +0300

    TodoDto

---
+++ src/main/java/com/silagedik/todo_project/business/dto/TodoDto.java
@@ -0,0 +1,29 @@
+package com.silagedik.todo_project.business.dto;
+
+import com.silagedik.todo_project.auditing.AuditingAwareBaseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import jakarta.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+
+//LOMBOK
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+public class TodoDto extends AuditingAwareBaseDto implements Serializable
+{
+    //serileştirme => bir veriyi başka bir yere taşırken herhangi bir sıkıntı yaşamamak için kullanırız
+    public static final Long serialVersionUID= 1L;
+
+   // private Long id;
+
+    @NotEmpty(message= "Todo cannot be empty")
+    private String title;
+    private boolean done;
+}
